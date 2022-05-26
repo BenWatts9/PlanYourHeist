4e1ac4e7fdda157ee69588b3fa5f3d9bc7060fe2
BenWatts9 <brandolphwatts@gmail.com>
2022-05-26 13:23:40 -0500

    phase-1

--- Program.cs
+++ Program.cs
@@ -6,7 +6,18 @@ namespace PlanYourHeist
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Plan Your Heist!");
+            Console.WriteLine();
+            Console.WriteLine("Please enter your team member's name:");
+            string memberName = Console.ReadLine();
+            Console.WriteLine("Please enter your team member's skill level:");
+            int memberSkillLevel = int.Parse(Console.ReadLine());
+            Console.WriteLine("Please enter your team member's courage factor:");
+            double memberCourageFactor = double.Parse(Console.ReadLine());
+
+            TeamMember newGuy = new TeamMember(memberName, memberSkillLevel, memberCourageFactor);
+
+            Console.WriteLine($"{newGuy.Name} has a skill level of {newGuy.SkillLevel} and a courage factor of {newGuy.CourageFactor}.");
         }
     }
 }
